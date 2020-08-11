45bb14b4ef5e637625edc27a1a860d99cdfa6056
Joe Previte <jprevite@fb.com>
2020-08-11 16:33:25 -0700

    feat: add second test

--- test/clojure_practice/functions_test.clj
+++ test/clojure_practice/functions_test.clj
@@ -2,6 +2,10 @@
     (:require [clojure.test :refer :all]
               [clojure-practice.functions :refer :all]))
 
+  (deftest annonymous-fn-test
+    (testing "returns the name interpolated into the string"
+      (is (= "hello, joe" ((fn [name] (str "hello, " name)) "joe")))))
+
   (deftest annonymous-fn-2-test
     (testing "adds two numbers together"
       (is (= 5 ((fn [a b] (+ a b)) 2 3)))))
