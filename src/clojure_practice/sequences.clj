53300260500a646caf40cce2c0e520b78ca18dff
Joe Previte <jprevite@fb.com>
2020-08-11 16:24:54 -0700

    fix: sequences files

---
+++ src/clojure_practice/sequences.clj
@@ -0,0 +1,9 @@
+(ns clojure-practice.sequences)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+
+
---
+++ test/clojure_practice/sequences_test.clj
@@ -0,0 +1,10 @@
+(ns clojure-practice.core-test
+    (:require [clojure.test :refer :all]
+              [clojure-practice.core :refer :all]))
+  
+  (deftest a-test
+    (testing "FIXME, I fail."
+      (is (= 1 1))))
+
+
+
