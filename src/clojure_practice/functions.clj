79c837bf155d06fe641694c41f0a0c9d552df997
Joe Previte <jprevite@fb.com>
2020-08-12 16:55:45 -0700

    feat: complete function exercises

--- src/clojure_practice/functions.clj
+++ src/clojure_practice/functions.clj
@@ -3,8 +3,8 @@
 ;; Instructions
 ; Practice writing functions in Clojure. Make sure you write tests too!
 ; - [x] Write 2 functions using the `fn` keyword: https://clojuredocs.org/clojure.core/fn
-; - [ ] Write 2 functions using the `defn` keyword: https://clojuredocs.org/clojure.core/defn
-; - [ ] Write 2 functions using the `def` keyword: https://clojuredocs.org/clojure.core/def
+; - [x] Write 2 functions using the `defn` keyword: https://clojuredocs.org/clojure.core/defn
+; - [x] Write 2 functions using the `def` keyword: https://clojuredocs.org/clojure.core/def
 
 ;; `fn` functions
 
@@ -13,9 +13,49 @@
 ; also, it looks like fn might be better suited for annoymous functions. Even though I gave it a name (helps with stack trace)
 ; it's not available in the scope outside of where it's defined.
 ((fn hello-person
-  [name]
-  (str "Hello, " + name)) "Joe")
+   [name]
+   (str "Hello, " + name)) "Joe")
 
 ; 2.
 ; here is a better example of an annonymous function and use case for fn
 ((fn [a b] (+ a b)) 2 3)
+
+
+;; `defn` functions
+
+; 1.
+; simple arthmitic
+
+
+(defn add-two
+  "Adds two to the number you pass in"
+  [n]
+  (+ n 2))
+
+; 2.
+; taking in two params
+(defn greet-user
+  "greets a user"
+  [greeting name]
+  (str greeting ", " name "!"))
+
+;; `def` functions
+
+; note to self: this is for defining a global variable...it's not special to functions, but can be used for them.
+
+; 1.
+
+(def subtract-ten
+  "Subtracts ten from a number"
+  (fn
+    [n]
+    (- n 10)))
+
+; 2.
+
+; yes this function is a bit pointless :)
+(def divide-by-one
+  "Divides by 1"
+  (fn
+    [n]
+    (/ n 1)))
--- test/clojure_practice/functions_test.clj
+++ test/clojure_practice/functions_test.clj
@@ -9,3 +9,19 @@
   (deftest annonymous-fn-2-test
     (testing "adds two numbers together"
       (is (= 5 ((fn [a b] (+ a b)) 2 3)))))
+
+  (deftest add-two-test
+    (testing "add-two actually adds two"
+      (is (= 12 (add-two 10)))))
+
+  (deftest greet-user-test
+    (testing "greets a user as expected"
+      (is (= "Good morning, Kona!" (greet-user "Good morning" "Kona")))))
+
+  (deftest subtract-ten-test
+    (testing "subtracts 10 as expected"
+      (is (= 1 (subtract-ten 11)))))
+
+  (deftest divide-by-one-test
+    (testing "divides by 1"
+      (is (= 12 (divide-by-one 12)))))
