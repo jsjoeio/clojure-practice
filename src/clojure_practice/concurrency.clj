61cf3a0211e450fd4ab601fd7cbd47131f1d2b19
Joe Previte <jjprevite@gmail.com>
2020-08-19 08:41:42 -0700

    feat: add concurrency module

---
+++ src/clojure_practice/concurrency.clj
@@ -0,0 +1,12 @@
+(ns clojure-practice.concurrency)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+; futures
+; delays
+; promises
+
+
+
---
+++ test/clojure_practice/concurrency_test.clj
@@ -0,0 +1,10 @@
+(ns clojure-practice.concurrency-test
+    (:require [clojure.test :refer :all]
+              [clojure-practice.concurrency :refer :all]))
+
+  (deftest a-test
+    (testing "FIXME, I fail."
+      (is (= 1 1))))
+
+
+
