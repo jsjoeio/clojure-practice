c5a600ba1d299e40e0e31390457694c1a5c51aae
Joe Previte <jjprevite@gmail.com>
2020-08-21 21:20:46 -0700

    feat: add some seq stuff

--- src/clojure_practice/sequences.clj
+++ src/clojure_practice/sequences.clj
@@ -10,6 +10,7 @@
 ; rest
 ; cons
 ; map
+; mapcat
 ; reduce
 ; filter
 ; distinct
@@ -33,4 +34,26 @@
 ; comp
 ; memoize
 ; update-in
+
+; 4Clojure #33 - Replicate a sequence
+; Write a function which replicates each element of a sequence a variable number of times.
+; i have two arguments: a sequence and number to repeat
+; what I'm thinking is we map over the sequence
+; repeat the number and take number times to repeat
+; tools: repeatedly and take
+
+(defn replicate-seq
+  "a function which replicates each element of a sequence a x number of times"
+  [s x]
+  (mapcat #(take x (repeat %)) s))
+
+; the issue i'm running into is that it creates a new list
+; and is adding that to the sequence
+
+(replicate-seq [1 2 3] 2)
+
+(take 2 (repeat [1 2]))
+;; be able to exp
+
+
 
